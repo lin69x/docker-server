2b1137e50734385a2c757bb6269e921b046f1105
Mihell <60088295+lin69x@users.noreply.github.com>
2022-11-07 21:46:58 +0400

    Update nginx.sh
    Restart container NGINX

--- nginx.sh
+++ nginx.sh
@@ -7,8 +7,14 @@ then
     docker-compose exec nginx nginx -t
 fi
 
-if [ "$1" = "restart" ]
+if [ "$1" = "reload" ]
 then
     echo '#Reload NGINX ...'
     docker-compose exec nginx nginx -s reload
+fi
+
+if [ "$1" = "restart" ]
+then
+    echo '#Restart container NGINX ...'
+    docker-compose restart nginx
 fi
