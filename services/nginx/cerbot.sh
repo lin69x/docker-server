3c1a1ff1ec3ee4d43c87c81ef7f7eb6e9dc92bda
Mihell <60088295+lin69x@users.noreply.github.com>
2023-06-16 23:58:33 +0400

    Update cerbot.sh

--- services/nginx/cerbot.sh
+++ services/nginx/cerbot.sh
@@ -8,11 +8,11 @@ else
         
         if [ "$1" = "prod" ]; then
             echo '#certbot ...'
-            certbot certonly --webroot --webroot-path=/var/www/letsencrypt --email lin69x@yandex.ru $2
+            certbot certonly --webroot --webroot-path=/var/www/html --email email@mail.ru $2
         elif [ "$1" = "renew" ]; then
-            certbot renew --webroot --webroot-path=/var/www/letsencrypt $2
+            certbot renew --webroot --webroot-path=/var/www/html $2
         elif [ "$1" = "dry" ]; then
-            certbot certonly --webroot --webroot-path=/var/www/letsencrypt --email lin69x@yandex.ru --dry-run $2
+            certbot certonly --webroot --webroot-path=/var/www/html --email email@mail.ru --dry-run $2
         else
             echo "Не понял что нужно сделать... - prod || renew || dry?"
         fi
